bbd8cf690e2a88bed48d4f1034c28acb0d9a9310
RgBerou <brotark@gmail.com>
2019-06-29 05:40:52 -0600

    Adding helper functions, routes, and views

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,14 @@
 const express=require('express');
-
+const path=require('path');
 const router=express.Router();
+
+const rootDir=require('../helper/path');
+
+
 
 router.get('/add-product',(req,res,next)=>{
 	console.log("In another middleware!");
-	res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');
+	res.sendFile(path.join(rootDir,'views','add-product.html'));
 });
 
 router.post('/add-product',(req,res,next)=>{
--- routes/shop.js
+++ routes/shop.js
@@ -3,9 +3,11 @@ const path=require('path');
 const express=require('express');
 const router=express.Router();
 
+const rootDir=require('../helper/path');
+
 
 router.get('/',(req,res,next)=>{
-	res.sendFile(path.join(__dirname,'../','views','shop.html'));
+	res.sendFile(path.join(rootDir,'views','shop.html'));
 });
 
 
