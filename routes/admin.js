b3d558027d273b9b48b2d1b527b083c22e492a03
rgberou <brotark@gmail.com>
2019-08-15 11:46:45 +0800

    Updated pug template

--- routes/admin.js
+++ routes/admin.js
@@ -10,7 +10,7 @@ router.get('/add-product',(req,res,next)=>{
 	console.log("In another middleware!");
 	//res.sendFile(path.join(rootDir,'views','add-product.html'));
 
-	res.render('add-product');
+	res.render('add-product',{pageTitle:"Add Product",path:"admin/add-product"});
 });
 
 router.post('/add-product',(req,res,next)=>{
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const adminData=require('./routes/admin');
 const shopRoutes=require('./routes/shop')
 
 app.set('view engine','pug');
+app.set('views','views');
 //adding body parser
 app.use(bodyParser.urlencoded({extended:false}));app.use(express.static(path.join(__dirname,'public')));
 //app.use('/admin',adminRoutes);
@@ -15,7 +16,8 @@ app.use('/admin',adminData.routes);
 app.use(shopRoutes);
 
 app.use((req,res,next)=>{
-	res.status(404).send('<h1>Page not found</h1>');
+	//res.status(404).send('<h1>Page not found</h1>');
+	res.status(404).render('error404');
 });
 
 
