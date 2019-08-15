8d4f207c3c3b069d651ea43ca3920bbb4c1a45d7
RgBerou <brotark@gmail.com>
2019-08-15 07:52:13 -0600

    Adding handlebars

--- routes/shop.js
+++ routes/shop.js
@@ -11,7 +11,7 @@ const adminData=require('./admin');
 router.get('/',(req,res,next)=>{
 	//console.log(adminData.products)
 	const products=adminData.products;
-	res.render('shop',{prods:products,docTitle:"Shop",path:"/"});
+	res.render('shop',{prods:products,pageTitle:"Shop",path:"/",hasProducts: products.length>0});
 	//res.sendFile(path.join(rootDir,'views','shop.html'));
 });
 
--- server.js
+++ server.js
@@ -2,13 +2,23 @@ const http=require('http');
 const path=require('path');
 const express=require('express');
 const bodyParser=require('body-parser');
+const expressHbs=require('express-handlebars');
+
 const app=express();
+
+
+//Adding handlebars
+app.engine('hbs',expressHbs())
+app.set('view engine','hbs');
+app.set('views','views');
+
+//Adding pug
+//app.set('view engine','pug');
+//app.set('views','views');
 
 const adminData=require('./routes/admin');
 const shopRoutes=require('./routes/shop')
 
-app.set('view engine','pug');
-app.set('views','views');
 //adding body parser
 app.use(bodyParser.urlencoded({extended:false}));app.use(express.static(path.join(__dirname,'public')));
 //app.use('/admin',adminRoutes);
@@ -17,7 +27,7 @@ app.use(shopRoutes);
 
 app.use((req,res,next)=>{
 	//res.status(404).send('<h1>Page not found</h1>');
-	res.status(404).render('error404');
+	res.status(404).render('error404',{pageTitle:"Page not found"});
 });
 
 
